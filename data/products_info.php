45c7f41805cd528c63cc16ea9eb3166f01c2cb66
patrictobola <tobola92@gmail.com>
2023-07-24 15:38:44 +0200

    done page structure

---
+++ data/products_info.php
@@ -0,0 +1,36 @@
+<?php
+$products = [
+    [
+        'name' => 'Royal Canin Mini Adult',
+        'url' => 'https://arcaplanet.vtexassets.com/arquivos/ids/243820/royal-canin-size-cane-mini-adult.jpg'
+    ],
+    [
+        'name' => 'Almo Nature Holistic Maintenance Medium Large Tonno e Riso',
+        'url' => 'https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg'
+    ],
+    [
+        'name' => 'Almo Nature Cat Daily Lattina',
+        'url' => 'https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg'
+    ],
+    [
+        'name' => 'Mangime per Pesci Guppy in Fiocchi',
+        'url' => 'https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg'
+    ],
+    [
+        'name' => 'Voliera Wilma in Legno',
+        'url' => 'https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg'
+    ],
+    [
+        'name' => 'Cartucce Filtranti per Filtro EasyCrystal',
+        'url' => 'https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg'
+    ],
+    [
+        'name' => 'Kong Classic',
+        'url' => 'https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg'
+    ],
+    [
+        'name' => 'Topini di peluche Trixie',
+        'url' => 'https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg'
+    ],
+
+];
