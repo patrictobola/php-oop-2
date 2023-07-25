bb4aea2446ee9c538b7e26cd2557468bcc8f31c9
patrictobola <tobola92@gmail.com>
2023-07-25 14:15:59 +0200

    fixed categories

---
+++ Models/Categories.php
@@ -0,0 +1,11 @@
+<?php
+class Categories
+{
+    public $animal;
+
+
+    public function __construct($animal)
+    {
+        $this->animal = $animal;
+    }
+}
--- Models/Products.php
+++ Models/Products.php
@@ -1,13 +1,14 @@
 <?php
-class Products
+include_once __DIR__ . '/Categories.php';
+class Products extends Categories
 {
     public $name;
     public $price;
-    public $animal;
     public $img_path;
 
     public function __construct($name, $price, $animal, $img_path)
     {
+        parent::__construct($animal);
         $this->name = $name;
         $this->price = $price;
         $this->animal = $animal;
